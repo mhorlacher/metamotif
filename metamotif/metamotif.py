97360bfe973bb6f16f7dd8dfab74be32a03ea08a
Marc Horlacher <marc.horlacher@gmail.com>
2021-09-14 23:32:49 +0200

    fixed bug

--- metamotif/metamotif.py
+++ metamotif/metamotif.py
@@ -8,17 +8,17 @@ from sklearn.cluster import DBSCAN
 from umap import UMAP
 
 # %%
-def extract_motifs(importances, k=7, norm=True):
+def extract_motif(importances, k=7, norm=True):
     importances = np.array(importances)
-    if len(importances.shape) != 3:
-        raise ValueError(f'Expected matrix with ndim=3, got ndim={len(importances.shape)}.')
+    if len(importances.shape) != 2:
+        raise ValueError(f'Expected matrix with ndim=2, got ndim={len(importances.shape)}.')
 
     best_i = -1
     best_score = -np.inf
 
     for i in range(0, importances.shape[0] - k + 1):
-        attributions_i = importances[i:(i+k), :]
-        score = np.sum(attributions_i)
+        importances_i = importances[i:(i+k), :]
+        score = np.sum(importances_i)
 
         if score > best_score:
             best_i = i
@@ -29,6 +29,14 @@ def extract_motifs(importances, k=7, norm=True):
         motif = motif / np.sum(motif)
 
     return motif
+
+# %%
+def extract_motifs(importances, k=7, norm=True):
+    importances = np.array(importances)
+    if len(importances.shape) != 3:
+        raise ValueError(f'Expected matrix with ndim=3, got ndim={len(importances.shape)}.')
+    
+    return np.stack([extract_motif(x, k=k, norm=norm) for x in importances])
 
 # %%
 def motifs_flatten(motifs_3d):
@@ -50,7 +58,7 @@ def extract_meta_motifs(importances, k=7, embed_fn=None, clust_fn=None):
     if clust_fn is None:
         clust_fn = lambda x: DBSCAN().fit(x).labels_
 
-    motifs = extract_motifs(importances, k=7, norm=True)
+    motifs = extract_motifs(importances, k=k, norm=True)
 
     motifs_n, motifs_len, motifs_depth = motifs.shape
 
@@ -62,7 +70,7 @@ def extract_meta_motifs(importances, k=7, embed_fn=None, clust_fn=None):
     motifs_df['cluster'] = motifs_clustering
 
     motifs_metaclusters = motifs_df.groupby(['cluster']).mean()
-    motifs_metaclusters = np.array(motifs_metaclusters).reshape(motifs_n, motifs_len, motifs_depth)
+    motifs_metaclusters = np.array(motifs_metaclusters).reshape(len(motifs_metaclusters), motifs_len, motifs_depth)
 
     return motifs_metaclusters
 
